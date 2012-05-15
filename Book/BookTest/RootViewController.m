1588164b8f0a9ec40d8f92bfd972ffd88b5616d3
Vincent LE JEUNE <vincent@lacompany.fr>
2012-05-15 06:58:27 +0200

    Cleaned out RootVC

--- Book/BookTest/RootViewController.m
+++ Book/BookTest/RootViewController.m
@@ -16,6 +16,8 @@
 
 @implementation RootViewController
 
+#define kTotalImages 24
+
 - (void)loadView
 {
     [super loadView];
@@ -23,35 +25,14 @@
     self.view = [[UIView alloc] init];
     self.view.backgroundColor = [UIColor grayColor];
         
-    /** ObjectRotation View **/
-    NSArray *imagesArray = [NSArray arrayWithObjects:
-                            [UIImage imageNamed:@"Renders_Test_360_0000.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0001.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0002.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0003.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0004.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0005.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0006.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0007.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0008.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0009.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0010.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0011.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0012.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0013.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0014.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0015.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0016.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0017.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0018.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0019.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0020.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0021.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0022.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0023.png"],
-                            [UIImage imageNamed:@"Renders_Test_360_0024.png"],
-                            nil];
-    /// could be placed in PList
+    /** 
+     * ObjectRotation View.
+     **/
+    NSMutableArray *imagesArray = [NSMutableArray arrayWithCapacity:kTotalImages];
+    for (int i = 0; i <= kTotalImages; i++) {
+        [imagesArray addObject:[UIImage imageNamed:[NSString stringWithFormat:@"Renders_Test_360_%.4d.png", i]]];
+    }
+    
     CGRect frame = CGRectMake(352, 264, 320, 240);
     
     KCObjectRotationView *objectRotationView = [[KCObjectRotationView alloc] initWithFrame:frame
@@ -63,7 +44,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
 }
 
 
